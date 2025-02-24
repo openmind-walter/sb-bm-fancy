a78152f50a0dc324468079c711a700c859365e71
isayas <167050730+isaacmulti@users.noreply.github.com>
2025-02-24 11:43:13 +0300

    fix:   market  cache   update

--- src/sb/services/fancy.update.service.ts
+++ src/sb/services/fancy.update.service.ts
@@ -26,7 +26,6 @@ export class FancyUpdateService {
             if (!fancyMarkets?.length) return;
             const wls = this.whiteLabelService.getActiveWhiteLabelsId();
 
-            for (let i = 0; i < wls.length; i++) {
                 await Promise.all(
                     fancyMarkets.map(async (market) => {
                         for (let i = 0; i < wls.length; i++) {
@@ -38,7 +37,7 @@ export class FancyUpdateService {
                         }
                     })
                 );
-            }
+    
         } catch (error) {
             this.logger.error(`processFancyMarketUpdates: ${error.message}`, FancyUpdateService.name);
         }
@@ -76,8 +75,8 @@ export class FancyUpdateService {
                 );
 
                 const updatedStoreFancyMarket = this.mergeFancyStoreMarkets(
+                    fancyMarket,
                     cahedFancyMarket,
-                    existingFancyMarket,
                     serviceId,
                     marketPubKey
                 );
